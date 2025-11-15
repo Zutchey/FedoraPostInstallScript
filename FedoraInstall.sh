7f56015f04e0a0f7d9c875a07885f40c50b1bd4c
Zutchey <79027616+Zutchey@users.noreply.github.com>
2025-11-15 02:29:34 -0800

    couple changes

--- FedoraInstall.sh
+++ FedoraInstall.sh
@@ -19,7 +19,6 @@ mkdir -p ~/.config/obs-studio/plugins ~/.config/kitty ~/.config/fastfetch
 # Download and install OBS plugin
 wget -qO- "https://github.com/dimtpap/obs-pipewire-audio-capture/releases/download/1.2.1/linux-pipewire-audio-1.2.1.tar.gz" | \
 tar -xzf - -C ~/.config/obs-studio/plugins
-rm -f ~/.config/obs-studio/plugins/linux-pipewire-audio-1.2.1.tar.gz
 
 #Swap FedoraFlatpak to FlathubFlatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -140,7 +139,7 @@ sudo systemctl enable --now docker.service
 sudo usermod -aG docker $USER
 
 #Install WinBoat (AppImage also exists)
-sudo rpm -i https://github.com/TibixDev/winboat/releases/download/v0.8.7/winboat-0.8.7-x86_64.rpm
+sudo dnf install -y https://github.com/TibixDev/winboat/releases/download/v0.8.7/winboat-0.8.7-x86_64.rpm
 
 # Install fonts
 sudo rpm --nodigest -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
