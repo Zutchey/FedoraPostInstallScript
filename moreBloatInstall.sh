1da90b1c508f40e613648400f9a048b139c59a15
Zutchey <79027616+Zutchey@users.noreply.github.com>
2025-11-23 20:24:59 -0800

    Create moreAppsInstall.sh

---
+++ moreBloatInstall.sh
@@ -0,0 +1,362 @@
+#!/bin/bash
+
+#Trap Sudo
+while true; do
+  sudo -v
+  sleep 300
+done &
+SUDO_PID=$!
+
+#Faster Package Downloads
+grep -qxF 'max_parallel_downloads=20' /etc/dnf/dnf.conf ||
+echo 'max_parallel_downloads=20' | sudo tee -a /etc/dnf/dnf.conf
+
+#More Stable Downloads
+grep -qxF 'fastestmirror=False' /etc/dnf/dnf.conf || \
+echo 'fastestmirror=False' | sudo tee -a /etc/dnf/dnf.conf
+
+#Create directories
+mkdir -p ~/.config/obs-studio/plugins ~/.config/kitty ~/.config/fastfetch ~/.config/MangoHud
+
+#Download and install OBS plugin
+wget -qO- "https://github.com/dimtpap/obs-pipewire-audio-capture/releases/download/1.2.1/linux-pipewire-audio-1.2.1.tar.gz" | \
+tar -xzf - -C ~/.config/obs-studio/plugins
+
+#Add Flathub Flatpaks
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+#Add repositories
+sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release -y
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf copr enable -y derisis13/ani-cli
+sudo dnf copr enable -y kylegospo/wallpaper-engine-kde-plugin
+
+#Multimedia Codecs
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
+sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
+sudo dnf swap mesa-vulkan-drivers mesa-vulkan-drivers-freeworld -y
+sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686 -y
+sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686 -y
+sudo dnf swap mesa-vulkan-drivers.i686 mesa-vulkan-drivers-freeworld.i686 -y
+
+#Install packages
+sudo dnf install -y --allowerasing --refresh steam
+sudo dnf install -y --allowerasing ani-cli 
+sudo dnf install -y --allowerasing trash-cli
+sudo dnf install -y --allowerasing cargo
+sudo dnf install -y --allowerasing java
+sudo dnf install -y --allowerasing cmatrix
+sudo dnf install -y --allowerasing fastfetch
+sudo dnf install -y --allowerasing gedit
+sudo dnf install -y --allowerasing pavucontrol
+sudo dnf install -y --allowerasing kitty
+sudo dnf install -y --allowerasing gnome-disk-utility
+sudo dnf install -y --allowerasing cowsay
+sudo dnf install -y --allowerasing fortune-mod
+sudo dnf install -y --allowerasing gimp
+sudo dnf install -y --allowerasing vlc
+sudo dnf install -y --allowerasing heroic
+sudo dnf install -y --allowerasing neovim
+sudo dnf install -y --allowerasing gnome-boxes
+sudo dnf install -y --allowerasing mpv
+sudo dnf install -y --allowerasing curl
+sudo dnf install -y --allowerasing cabextract
+sudo dnf install -y --allowerasing xorg-x11-font-utils
+sudo dnf install -y --allowerasing fontconfig
+sudo dnf install -y --allowerasing qbittorrent
+sudo dnf install -y --allowerasing obs-studio
+sudo dnf install -y --allowerasing umu-launcher
+sudo dnf install -y --allowerasing lutris
+sudo dnf install -y --allowerasing goverlay
+sudo dnf install -y --allowerasing mangohud
+sudo dnf install -y --allowerasing docker
+sudo dnf install -y --allowerasing docker-compose
+sudo dnf install -y --allowerasing testdisk
+sudo dnf install -y --allowerasing winetricks
+sudo dnf install -y --allowerasing wine
+sudo dnf install -y --allowerasing wine-core
+sudo dnf install -y --allowerasing chromium
+sudo dnf install -y --allowerasing falcond
+sudo dnf install -y --allowerasing gnome-tweaks
+sudo dnf install -y --allowerasing g4music
+sudo dnf install -y --allowerasing kdenlive
+sudo dnf install -y --allowerasing krita
+sudo dnf install -y --allowerasing wallpaper-engine-kde-plugin
+sudo dnf install -y --allowerasing protontricks
+sudo dnf install -y --allowerasing htop
+sudo dnf install -y --allowerasing bison
+sudo dnf install -y --allowerasing obs-studio-plugin-vkcapture
+sudo dnf install -y --allowerasing cpu-x
+sudo dnf install -y --allowerasing pokeget
+sudo dnf install -y --allowerasing btrfs-assistant
+sudo dnf install -y --allowerasing snapper
+sudo dnf install -y --allowerasing discord
+sudo dnf install -y --allowerasing flex
+sudo dnf install -y --allowerasing rocm-opencl
+sudo dnf install -y --allowerasing libxcrypt-compat
+sudo dnf install -y --allowerasing libcurl
+sudo dnf install -y --allowerasing libcurl-devel
+sudo dnf install -y --allowerasing mesa-libGLU
+sudo dnf install -y --allowerasing freerdp
+sudo dnf install -y --allowerasing libva-utils
+sudo dnf install -y --allowerasing kernel-devel
+
+#Install Core
+sudo dnf group install core -y
+
+#Enable services
+sudo systemctl enable --now falcond
+sudo systemctl enable --now docker.service
+sudo usermod -aG docker $USER
+
+#Install WinBoat (AppImage also exists)
+sudo dnf install -y https://github.com/TibixDev/winboat/releases/download/v0.8.7/winboat-0.8.7-x86_64.rpm
+
+#Install fonts
+sudo rpm --nodigest -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FantasqueSansMono.zip
+unzip -q FantasqueSansMono.zip -d ~/.local/share/fonts/
+rm FantasqueSansMono.zip
+fc-cache -f
+
+#System configuration
+echo 'OBS_VKCAPTURE=1' | sudo tee -a /etc/environment
+echo 'MANGOHUD=1' | sudo tee -a /etc/environment
+
+#Gnome Changes
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
+#Install flatpaks
+flatpak install -y --system flathub \
+    it.mijorus.gearlever \
+    org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08 \
+    io.missioncenter.MissionCenter \
+    org.localsend.localsend_app \
+    com.spotify.Client \
+    dev.bragefuglseth.Keypunch \
+    org.nickvision.tubeconverter \
+    com.dec05eba.gpu_screen_recorder \
+    io.gitlab.theevilskeleton.Upscaler \
+    garden.jamie.Morphosis \
+    com.usebottles.bottles \
+    com.github.wwmm.easyeffects \
+    fr.handbrake.ghb \
+    com.protonvpn.www \
+    me.proton.Pass \
+    io.github.sigmasd.stimulator \
+    com.mattjakeman.ExtensionManager \
+    ca.desrt.dconf-editor \
+    io.github.flattool.Ignition \
+    com.github.tchx84.Flatseal \
+    org.vinegarhq.Sober \
+    com.vysp3r.ProtonPlus \
+    io.github.ilya_zlobintsev.LACT \
+    io.github.ryubing.Ryujinx \
+    com.valvesoftware.Steam.CompatibilityTool.Proton-GE \
+    org.azahar_emu.Azahar \
+    sh.ppy.osu
+
+flatpak repair
+
+#Flatpak Permission Fixes
+flatpak override --user --filesystem=/home/$USER/.icons/:ro
+flatpak override --user --filesystem=/usr/share/icons/:ro
+flatpak override --user --env=XCURSOR_PATH=$HOME/.icons
+flatpak override --user --filesystem=xdg-config/MangoHud:ro
+
+#Enable Wine NTsync
+cat << EOF | sudo tee /etc/modules-load.d/ntsync.conf > /dev/null
+ntsync
+EOF
+
+#Configure shell
+grep -qF 'alias system-update' ~/.bashrc || cat << 'EOF' >> ~/.bashrc
+export PATH="$HOME/.cargo/bin:$PATH"
+
+if [[ $- == *i* ]]; then
+    pokeget random --hide-name | fastfetch --file-raw -
+fi
+
+alias system-update='sudo dnf update -y --refresh && flatpak update -y'
+EOF
+
+#Configure Kitty
+cat << 'EOF' > ~/.config/kitty/kitty.conf
+font_family FantasqueSansM Nerd Font Mono Bold
+font_size 16.0
+bold_font auto
+italic_font auto
+bold_italic_font auto
+
+background_opacity 0.9
+dynamic_background_opacity 1
+confirm_os_window_close 0
+
+cursor_trail 1
+linux_display_server auto
+scrollback_lines 2000
+wheel_scroll_min_lines 1
+enable_audio_bell no
+window_padding_width 4
+
+selection_foreground none
+selection_background none
+
+foreground #dddddd
+background #000000
+cursor #dddddd
+EOF
+
+#Configure Fastfetch
+cat << 'EOF' > ~/.config/fastfetch/config.jsonc
+{
+"$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
+"logo": {"padding": {"top": 1}},
+"display": {"separator": " 󰑃  "},
+"modules": [
+    "break",
+    {
+    "type": "title",
+    "format": "{#1}╭───────────── {#}{user-name-colored}",
+    "formatColor": "yellow"
+    },
+    {"type": "os", "key": " DISTRO", "keyColor": "yellow"},
+    {"type": "kernel", "key": "│ ├", "keyColor": "yellow"},
+    {"type": "packages", "key": "│ ├󰏖", "keyColor": "yellow"},
+    {"type": "shell", "key": "│ └", "keyColor": "yellow"},
+    {"type": "de", "key": "󰧨 DE/WM", "keyColor": "blue"},
+    {"type": "wm", "key": "│ ├", "keyColor": "blue"},
+    {"type": "wmtheme", "key": "│ ├󰉼", "keyColor": "blue"},
+    {"type": "icons", "key": "│ ├󰀻", "keyColor": "blue"},
+    {"type": "cursor", "key": "│ ├", "keyColor": "blue"},
+    {"type": "terminalfont", "key": "│ ├", "keyColor": "blue"},
+    {"type": "terminal", "key": "│ ├", "keyColor": "blue"},
+    {"type": "uptime", "key": "│ └󰅐", "keyColor": "blue"},
+    {"type": "host", "key": "󰌢 SYSTEM", "keyColor": "green"},
+    {"type": "cpu", "key": "│ ├󰻠", "keyColor": "green"},
+    {"type": "gpu", "key": "│ ├󰢮", "format": "{2}", "keyColor": "green"},
+    {"type": "memory", "key": "│ ├󰍛", "keyColor": "green"},
+    {"type": "disk", "key": "│ ├󰋊", "keyColor": "green"},
+    {"type": "display", "key": "│ └󰍹", "keyColor": "green", "compactType": "original-with-refresh-rate"},
+    {"type": "sound", "key": " AUDIO", "format": "{2}", "keyColor": "magenta"},
+    "break"
+    ]
+}
+EOF
+
+#Configure MangoHud
+cat << 'EOF' > ~/.config/MangoHud/MangoHud.conf
+################### File Generated by Goverlay ###################
+legacy_layout=false
+
+background_alpha=0.6
+round_corners=10
+background_alpha=0.6
+background_color=000000
+
+font_size=24
+text_color=FFFFFF
+position=top-left
+toggle_hud=Shift_R+F12
+no_display
+pci_dev=0:03:00.0
+table_columns=3
+gpu_text=GPU
+gpu_stats
+gpu_temp
+gpu_power
+gpu_color=2E9762
+cpu_text=CPU
+cpu_stats
+core_load
+core_bars
+cpu_temp
+cpu_power
+cpu_color=FF0000
+swap
+ram
+ram_color=C26693
+fps
+gpu_name
+vulkan_driver
+wine
+wine_color=EB5B5B
+frame_timing
+frametime_color=00FF00
+fps_limit_method=late
+toggle_fps_limit=Shift_L+F1
+
+fps_limit=0
+resolution
+winesync
+custom_text=Session:
+exec=echo $XDG_SESSION_TYPE
+#offset=0
+
+output_folder=~/Documents
+log_duration=30
+autostart_log=0
+log_interval=100
+toggle_logging=Shift_L+F2
+log_versioning
+upload_logs
+
+blacklist=pamac-manager,lact,ghb,bitwig-studio,ptyxis,yumex
+EOF
+
+# Wooting Support
+sudo tee /etc/udev/rules.d/70-wooting.rules > /dev/null << 'EOF'
+# Wooting One Legacy
+SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
+SUBSYSTEM=="usb",    ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
+
+# Wooting One update mode
+SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2402", TAG+="uaccess"
+
+# Wooting Two Legacy
+SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
+SUBSYSTEM=="usb",    ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
+
+# Wooting Two update mode
+SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2403", TAG+="uaccess"
+
+# Generic Wooting devices
+SUBSYSTEM=="hidraw", ATTRS{idVendor}=="31e3", TAG+="uaccess"
+SUBSYSTEM=="usb",    ATTRS{idVendor}=="31e3", TAG+="uaccess"
+EOF
+
+# Reload udev rules
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+
+#Change computer name
+sudo hostnamectl set-hostname fedora-pc
+
+#Kill sudo loop
+kill "$SUDO_PID"
+
+#Enable Steam h.264 (Auto closes after 3 minutes)
+steam steam://unlockh264/ & sleep 180; kill $(pgrep steam)
+
+echo "Setup complete! Reboot recommended."
+while true; do
+    printf "Reboot now? (y/n) "
+    read REPLY
+
+    case "$REPLY" in
+        [Yy])
+            reboot
+            break
+            ;;
+        [Nn])
+            echo "Not rebooting."
+            break
+            ;;
+        *)
+            echo "Invalid input. Please enter y or n."
+            ;;
+    esac
+done
