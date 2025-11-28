531210f08ec102f264aa04e64bafe1bbacce0bb3
Zutchey <79027616+Zutchey@users.noreply.github.com>
2025-11-28 06:08:18 -0800

    remove custom configs

--- InstallScript.sh
+++ InstallScript.sh
@@ -71,7 +71,6 @@ sudo dnf install -y --allowerasing cargo
 sudo dnf install -y --allowerasing java
 sudo dnf install -y --allowerasing fastfetch
 sudo dnf install -y --allowerasing pavucontrol
-sudo dnf install -y --allowerasing kitty
 sudo dnf install -y --allowerasing gnome-disk-utility
 sudo dnf install -y --allowerasing gimp
 sudo dnf install -y --allowerasing vlc
@@ -174,75 +173,7 @@ EOF
 grep -qF 'alias system-update' ~/.bashrc || cat << 'EOF' >> ~/.bashrc
 export PATH="$HOME/.cargo/bin:$PATH"
 
-if [[ $- == *i* ]]; then
-    pokeget random --hide-name | fastfetch --file-raw -
-fi
-
 alias system-update='sudo dnf update -y --refresh && flatpak update -y'
-EOF
-
-#Configure Kitty
-cat << 'EOF' > ~/.config/kitty/kitty.conf
-font_family FantasqueSansM Nerd Font Mono Bold
-font_size 16.0
-bold_font auto
-italic_font auto
-bold_italic_font auto
-
-background_opacity 0.9
-dynamic_background_opacity 1
-confirm_os_window_close 0
-
-cursor_trail 1
-linux_display_server auto
-scrollback_lines 2000
-wheel_scroll_min_lines 1
-enable_audio_bell no
-window_padding_width 4
-
-selection_foreground none
-selection_background none
-
-foreground #dddddd
-background #000000
-cursor #dddddd
-EOF
-
-#Configure Fastfetch
-cat << 'EOF' > ~/.config/fastfetch/config.jsonc
-{
-"$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
-"logo": {"padding": {"top": 1}},
-"display": {"separator": " 󰑃  "},
-"modules": [
-    "break",
-    {
-    "type": "title",
-    "format": "{#1}╭───────────── {#}{user-name-colored}",
-    "formatColor": "yellow"
-    },
-    {"type": "os", "key": " DISTRO", "keyColor": "yellow"},
-    {"type": "kernel", "key": "│ ├", "keyColor": "yellow"},
-    {"type": "packages", "key": "│ ├󰏖", "keyColor": "yellow"},
-    {"type": "shell", "key": "│ └", "keyColor": "yellow"},
-    {"type": "de", "key": "󰧨 DE/WM", "keyColor": "blue"},
-    {"type": "wm", "key": "│ ├", "keyColor": "blue"},
-    {"type": "wmtheme", "key": "│ ├󰉼", "keyColor": "blue"},
-    {"type": "icons", "key": "│ ├󰀻", "keyColor": "blue"},
-    {"type": "cursor", "key": "│ ├", "keyColor": "blue"},
-    {"type": "terminalfont", "key": "│ ├", "keyColor": "blue"},
-    {"type": "terminal", "key": "│ ├", "keyColor": "blue"},
-    {"type": "uptime", "key": "│ └󰅐", "keyColor": "blue"},
-    {"type": "host", "key": "󰌢 SYSTEM", "keyColor": "green"},
-    {"type": "cpu", "key": "│ ├󰻠", "keyColor": "green"},
-    {"type": "gpu", "key": "│ ├󰢮", "format": "{2}", "keyColor": "green"},
-    {"type": "memory", "key": "│ ├󰍛", "keyColor": "green"},
-    {"type": "disk", "key": "│ ├󰋊", "keyColor": "green"},
-    {"type": "display", "key": "│ └󰍹", "keyColor": "green", "compactType": "original-with-refresh-rate"},
-    {"type": "sound", "key": " AUDIO", "format": "{2}", "keyColor": "magenta"},
-    "break"
-    ]
-}
 EOF
 
 #Configure MangoHud
