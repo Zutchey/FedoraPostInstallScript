14d1d2e9a1517f9db651d3c6227be6687bad1204
Zutchey <79027616+Zutchey@users.noreply.github.com>
2025-11-28 01:21:17 -0800

    Update InstallScript.sh

--- InstallScript.sh
+++ InstallScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Log Terminal Output
-LOGFILE="/home/jesus/Downloads/FedoraPostInstallScript/fedora-setup.log"
+LOGFILE="/home/jesus/Downloads/FedoraPostInstallScript/InstallScript.log"
 exec > >(tee -a "$LOGFILE") 2>&1
 
 #Trap Sudo
